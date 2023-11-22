2d9a6fdf4a955ec384d99d488406eaf31c988f65
samcasubuan <s.mcasubuan23@yahoo.com>
2023-11-22 13:47:05 +0800

    end task 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGallery = (el) => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>
+
+  `;
+    })
+    .join("");
+};
+
+const photoMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photoMarkup);
+
+//---------------------------------------------------------
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox");
